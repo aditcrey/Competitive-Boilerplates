b62abd9a794d49fa9b5dc250e8833efe4bb1186c
Aditya Thakre <aditcrey@gmail.com>
2018-08-07 12:08:10 +0530

    better legibility

--- Largest_Rectangle_in_Histogram.cpp
+++ Largest_Rectangle_in_Histogram.cpp
@@ -1,26 +1,30 @@
-int maxAreaUnderHist(vector<int> hist){
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        if(n==0) return 0;
         stack<int> stk;
-        int n =hist.size();
         int i=0;
         int max_area = 0;
         while(i<n){
 
-            if(stk.empty()) {stk.push(i); i++;}
-            else{
-                if(hist[stk.top()] <= hist[i]) {stk.push(i); i++;}
-                else{
-                    int tp = stk.top();
-                    stk.pop();
-                    max_area = max(max_area, hist[tp] * (stk.empty()?i:i-stk.top()-1));
-                }
+            if(stk.empty() || heights[stk.top()] <= heights[i]){
+                stk.push(i);
+                i++;
+            }else{
+                int tp = stk.top();
+                stk.pop();
+                max_area = max(max_area, heights[tp] * (stk.empty()?i:i-stk.top()-1));
             }
+            
         }
         
         while(!stk.empty()){
-
             int tp = stk.top();
             stk.pop();
-            max_area = max(max_area, hist[tp]* (stk.empty()?i:i-stk.top()-1));   
+            max_area = max(max_area, heights[tp]*  (stk.empty()?i:i-stk.top()-1));
         }
+        
         return max_area;
     }
+};
